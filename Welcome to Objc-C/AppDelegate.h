6239bb48e15fce1d4a4227ef79c3bf3808fb2f77
YesVladess <vladess444@yandex.ru>
2021-11-05 17:44:57 +0300

    Initial Commit

---
+++ Welcome to Objc-C/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Welcome to Objc-C
+//
+//  Created by YesVladess on 05.11.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Welcome to Objc-C/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Welcome to Objc-C
+//
+//  Created by YesVladess on 05.11.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
