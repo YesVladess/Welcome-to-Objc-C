e22382e7b0bd34178fe14e56b337f957bf62b6c5
YesVladess <vladess444@yandex.ru>
2022-03-13 17:48:11 +0300

    Loop lesson

---
+++ Welcome to Objc-C/Lessons/DeathLoop.h
@@ -0,0 +1,16 @@
+//
+//  DeathLoop.h
+//  Welcome to Objc-C
+//
+//  Created by YesVladess on 13.03.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DeathLoop : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Welcome to Objc-C/Lessons/DeathLoop.m
@@ -0,0 +1,12 @@
+//
+//  DeathLoop.m
+//  Welcome to Objc-C
+//
+//  Created by YesVladess on 13.03.2022.
+//
+
+#import "DeathLoop.h"
+
+@implementation DeathLoop
+
+@end
